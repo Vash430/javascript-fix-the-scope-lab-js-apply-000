771ae04b53ca8ece10c80dd5ba17fa5849f506b5
essential-form-0157 <vasmith430@gmail.com>
2020-07-04 02:44:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
+  var animail = "cat";
   return animal
 }
 
